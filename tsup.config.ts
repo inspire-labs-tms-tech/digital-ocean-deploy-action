bb03b98c55a5d00fb5c30db5930dcda6bf221430
Nicholas Barrow <me@nicholasrbarrow.com>
2024-04-06 16:46:13 -0400

    bug fixes and typescript migration

--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["cjs"], // Build for commonJS and ESmodules
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-});
